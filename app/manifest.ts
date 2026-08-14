548d0c05cb1f38df97e7c10c0f7b0b21a84f7a8c
Dhruv Tapaniya <tapaniyadhruv007@gmail.com>
2026-08-14 14:08:07 +0530

    fix: correct manifest icons sizes for PWA

--- app/manifest.ts
+++ app/manifest.ts
@@ -19,11 +19,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icon.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'any maskable'
       },
       {
-        src: '/icon.png',
+        src: '/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'any maskable'
       }
     ],
   };
